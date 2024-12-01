a1fe03b3534bf815731f6a96bc29818c12da5477
kushmdesai <kushmdesai@icloud.com>
2024-11-30 20:50:27 -0500

    first commit

---
+++ mqtt_esp8266/README.md
@@ -0,0 +1,14 @@
+## ESP8266 Project
+
+## Setup
+
+### Setup RaspberyPI
+1. Download the imager
+```
+https://downloads.raspberrypi.org/imager/imager_latest.exe
+```
+2. 
+
+## Execute
+
+
---
+++ mqtt_esp8266/workin_pir/workin_pir.ino
@@ -0,0 +1,157 @@
+/*
+ Basic ESP8266 MQTT example
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "ATTKz3jkTA";
+const char* password = "bvzv4exam#2m";
+const char* mqtt_server = "kmdprojects";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+int sensorPowerToggle = 1;
+int pirSensor = D1;
+int msgValue = 1;
+int state = LOW;
+int val = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    sensorPowerToggle = 1;
+
+  } else {
+    sensorPowerToggle = 0;
+    client.publish("motionDetectionStatus", "no more motion :) :(");
+    Serial.print("no more motioin");
+  }
+  
+
+}
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outzyxMqttTopic", "hello world");
+      Serial.print("hello world sent");
+      // ... and resubscribe
+      client.subscribe("inxyzMessageTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(pirSensor, INPUT);     // Initialize the sensorPowerToggle pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void motionDetecter(){
+  val = digitalRead(pirSensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    delay(100);                // delay 100 milliseconds 
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      client.publish("motionDetectionStatus", "OMG! THERE IS MOTION");
+      state = HIGH;       // update variable state to HIGH
+    }
+  } 
+  else {
+      delay(200);             // delay 200 milliseconds 
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        client.publish("motionDetectionStatus", "no more motion :( :)");
+        state = LOW;       // update variable state to LOW
+    }
+  }
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  if (sensorPowerToggle == 1){
+    unsigned long now = millis();
+    if (now - lastMsg > 2000) {
+      lastMsg = now;
+      motionDetecter();
+    }
+  }  
+}
+
+
